dfbdc977abd5bb9a4d29f7a574436654e7b1b0ef
IamPranit <maskepranit@gmail.com>
2018-07-22 15:52:40 +0530

    Adding heroku env database url

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'postgres',
-    database : 'face'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
